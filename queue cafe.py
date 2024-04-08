2a4abc90a6b014a73c70390f4c3cc899e76bd48c
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-04-08 20:14:54 +0300

    Про кафе

--- queue cafe.py
+++ queue cafe.py
@@ -2,34 +2,40 @@ import threading
 import time
 import queue
 
+
 class Table:
-    def __init__(self, number):
+    def __init__(self, number=None):
         self.number = number
         self.is_busy = False
 
 class Cafe:
     def __init__(self, tables):
         self.tables = tables
-        self.queue = queue.Queue()
+        self.queue = queue.Queue(maxsize=3)
 
     def client_come_in(self):
         for i in range(1, 21):
             time.sleep(1)
             print(f"Посетитель номер {i} прибыл")
             customer = Customer(i)
-            self.service(customer)
+            self.service([customer])
 
-    def service(self, customer):
-        for table in self.tables:
-            if not table.is_busy:
-                table.is_busy = True
+    def service(self, customers):
+        free_tables = [table for table in self.tables if not table.is_busy]
+        if len(free_tables) >= 3:
+            table = free_tables[0]
+            table.is_busy = True
+            for customer in customers:
                 print(f"Посетитель номер {customer.id} сел за стол {table.number} (начало обслуживания)")
-                time.sleep(5)
+            time.sleep(5)
+            for customer in customers:
                 print(f"Посетитель номер {customer.id} покушал и ушёл (конец обслуживания)")
-                table.is_busy = False
-                return
-        print(f"Посетитель номер {customer.id} ожидает свободный стол (помещение в очередь)")
-        self.queue.put(customer)
+            table.is_busy = False
+        else:
+            print("Нет доступных столиков для обслуживания.")
+            for customer in customers:
+                self.queue.put(customer)
+
 
 class Customer(threading.Thread):
     def __init__(self, id):
